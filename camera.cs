e7c61cbfef11710c870e54d3f0b833ceb37eca0f
yaziid <cantinanv@hotmail.com>
2014-11-14 23:12:41 +0100

    Update camera.cs

--- camera.cs
+++ camera.cs
@@ -1,55 +1,18 @@
 using UnityEngine;
 using System.Collections;
 
-public class deplacement : MonoBehaviour {
+public class camera_dep : MonoBehaviour {
 
-	public float speed = 5.5f;
-	public float jump = 6.5f;
-
-	private bool coli = false; // test de colissions pour jump
+	public GameObject player;
+	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		print("Objet initialisé !");
+		offset = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		deplacements();
-	
-	}
-
-	void OnCollisionEnter(Collision col)// quand il ya une colli
-	{
-		coli = true;
-	}
-
-	public void deplacements()
-	{
-		if(Input.GetKey(KeyCode.UpArrow))
-		{
-			transform.position += new Vector3(0, 0, speed)* Time.deltaTime;
-		}
-		if(Input.GetKey(KeyCode.DownArrow))
-		{
-			transform.position += new Vector3(0, 0, -speed)* Time.deltaTime;
-		}
-		if(Input.GetKey(KeyCode.RightArrow))
-		{
-			transform.position += new Vector3(speed, 0, 0)* Time.deltaTime;
-		}
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			transform.position += new Vector3(-speed, 0, 0)* Time.deltaTime;
-		}
-		if(Input.GetKey(KeyCode.Space))
-		{
-			if(coli)// test de coli pour jump
-			{
-				rigidbody.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
-				coli = false;
-			}
-		}
+		transform.position = player.transform.position + offset;
 	}
 }
